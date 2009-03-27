d862e9ddd8f84fea37a16a7a82d9901822014e43
Eric Stratmann <estrat@stanford.edu>
2009-03-27 02:40:58 +0000

    FIZ-33: Added ImageSelector class and tests

---
+++ src/org/fiz/ImageSelector.java
@@ -0,0 +1,94 @@
+package org.fiz;
+
+/**
+ * An ImageSelector outputs HTML for one of several images, chosen based on a 
+ * data value and a mapping dataset. 
+ * <p>
+ * ImageSelector supports the following properties:
+ * map -	(required) A dataset of mappings. See below for format.
+ * id -		(required) Name of column in dataset we are mapping to an image
+ * <p>
+ * The map dataset contains key - value pairs, where a key is one of the
+ * possible strings we will use to select an image. A value can either be a 
+ * string in which case it represents the src tag for an image, or it can be a 
+ * Dataset which contains src and alt elements, describing the URL for the
+ * image and an alt tag. Both src and alt values are expanded as templates.
+ * Map recognizes one special key, which is "default". If that key exists, it 
+ * is used to find the default image if a particular key is not found.
+ */
+public class ImageSelector implements Formatter {
+	
+	/* Constructor properties */
+    protected String id;
+    protected Dataset map;
+
+	protected static String DEFAULT_KEY = "default";
+
+	/**
+	 * Construct an ImageSelector object with the given set of properties.
+	 * @param properties		A collection of values describing the
+	 * 							configuration of the ImageSelector; see above
+	 *							for the supported values.
+	 */
+    public ImageSelector(Dataset properties) {
+		map = properties.getChild("map");
+		id = properties.get("id");
+    }
+
+	/**
+	 * Construct an ImageSelector given values for the {@code id} and {@code map}
+	 * properties.
+	 * @param id				Value for the {@code id} property.
+	 * @param map				Value for the {@code map} property.
+	 */
+	public ImageSelector(String id, Dataset map) {
+		this.id = id;
+		this.map = map;
+	}
+
+	/**
+	 * Generates HTML for the the ImageSelector, using properties passed to the
+	 * constructor and data passed into this method
+     * @param cr             Overall information about the client
+     *                       request being serviced.
+     * @param data           Values in this dataset are used to expand 
+	 * 						 templates. We also use {@code id} passed to the 
+	 * 						 constuctor to reference a column in this row which
+	 * 						 is used to select an image to display.
+     * @param out            HTML for the ImageSelector is appended here.
+	 */
+    public void html(ClientRequest cr, Dataset data, StringBuilder out) {
+		
+		String src, alt = null;
+		String key = data.get(id);
+		Object value = map.lookup(key);
+
+		if (value == null) {
+			value = map.lookup(DEFAULT_KEY);
+
+			if (value == null) {
+				throw new Dataset.MissingValueError(key);
+			}
+		}
+
+		if (value instanceof String) {
+			src = (String) value;
+		} else if (value instanceof Dataset) {
+			Dataset imgData = (Dataset) value;
+			src = imgData.get("src");
+			alt = imgData.check("alt");
+		} else {
+			throw new InternalError("ImageSelector does not support class " +
+				value.getClass().getSimpleName() + ". It only supports " + 
+				"string and dataset values. ");
+		}
+			
+		if (alt == null) {
+			alt = key;
+		}
+
+		Template.expand("<img src=\"@1\" alt=\"@2\" />", out,
+			Template.expand(src, data, Template.SpecialChars.URL),
+			Template.expand(alt, data, Template.SpecialChars.NONE));
+    }
+}
---
+++ test/org/fiz/ImageSelectorTest.java
@@ -0,0 +1,129 @@
+package org.fiz;
+
+/**
+ * Junit tests for the ImageSelector class
+ */
+
+public class ImageSelectorTest extends junit.framework.TestCase {
+	protected ClientRequest cr;
+	protected StringBuilder out;
+	protected Dataset testRow;
+	protected ImageSelector is;
+
+	public void setUp() {
+		cr = new ClientRequestFixture();
+		out = new StringBuilder();
+		testRow = new Dataset("foo", "bar");
+	}
+
+
+	public void test_constructor_properties() {
+		Dataset data = new Dataset("id", "foo", "map", 
+			new Dataset("bar", "baz"));
+		is = new ImageSelector(data);
+		assertEquals("foo", is.id);
+		assertEquals("baz", is.map.get("bar"));
+	}
+
+	public void test_constructor_emptyProperties() {
+		try {
+			is = new ImageSelector(new Dataset());
+		} catch (Dataset.MissingValueError e) {
+			return;
+		}
+		fail("Expected exception not thrown");
+	}
+
+	public void test_constructor_separate() {
+		is = new ImageSelector("foo", new Dataset("bar", "baz"));
+		assertEquals("foo", is.id);
+		assertEquals("baz", is.map.get("bar"));
+	}
+
+	public void test_html_keyNotInDataset() {
+    	Dataset data = new Dataset("id", "baz", "map", new Dataset());
+		is = new ImageSelector(data);
+		try {
+			is.html(cr, testRow, out);
+		} catch (Dataset.MissingValueError e) {
+			assertEquals("couldn't find dataset element \"baz\"",
+				e.getMessage());
+			return;
+		}
+		fail("Expected exception not thrown");
+	}
+
+	public void test_html_keyNotFoundNoDefault() {
+		Dataset data = new Dataset("id", "foo", "map", new Dataset());
+		is = new ImageSelector(data);
+		try {
+			is.html(cr, testRow, out);
+		} catch (Dataset.MissingValueError e) {
+			assertEquals("couldn't find dataset element \"bar\"",
+				e.getMessage());
+			return;
+		}
+		fail("Expected exception not thrown");
+	}
+
+	public void test_html_keyNotFoundUseDefault() {
+		Dataset data = new Dataset("id", "foo", "map", new Dataset(
+				"default", "default.png"));
+		is = new ImageSelector(data);
+		is.html(cr, testRow, out);
+		assertEquals("<img src=\"default.png\" alt=\"bar\" />", 
+				out.toString());
+
+	}
+
+	public void test_html_valueIsString() {
+		Dataset data = new Dataset("id", "foo", "map", new Dataset(
+				"bar", "bar.png"));
+		is = new ImageSelector(data);
+		is.html(cr, testRow, out);
+		assertEquals("<img src=\"bar.png\" alt=\"bar\" />", out.toString());
+	}
+
+	public void test_html_valueIsDatasetNoSrc() {
+		Dataset data = new Dataset("id", "foo", "map", new Dataset(
+				"bar", new Dataset()));
+		is = new ImageSelector(data);
+		try {
+			is.html(cr, testRow, out);
+		} catch (Dataset.MissingValueError e) {
+			assertEquals("couldn't find dataset element \"src\"",
+				e.getMessage());
+			return;
+		}
+		fail("Expected exception not thrown");
+	}
+
+	public void test_html_valueIsDatasetWithAlt() {
+		Dataset data = new Dataset("id", "foo", "map", new Dataset(
+				"bar", new Dataset(
+				"src", "bar.png", "alt", "baz")));
+		is = new ImageSelector(data);
+		is.html(cr, testRow, out);
+		assertEquals("<img src=\"bar.png\" alt=\"baz\" />",	out.toString());
+	}
+
+	public void test_html_valueIsDatasetWithoutAlt() {
+		Dataset data = new Dataset("id", "foo", "map", new Dataset(
+				"bar", new Dataset("src", "bar.png")));
+		is = new ImageSelector(data);
+		is.html(cr, testRow, out);
+		assertEquals("<img src=\"bar.png\" alt=\"bar\" />",	out.toString());
+	}
+
+	public void test_html_useTemplate() {
+		Dataset map = new Dataset("bar", new Dataset(
+				"src", "@a", "alt", "@a"));
+
+		// It's a weird template to make sure escaping works properly
+		testRow.set("a", "a <\"~");
+		is = new ImageSelector("foo", map);
+		is.html(cr, testRow, out);
+		assertEquals("<img src=\"a+%3c%22%7e\" alt=\"a &lt;&quot;~\" />",
+			out.toString());
+	}
+}
