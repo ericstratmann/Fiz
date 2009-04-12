e807f164ece5091a98d44056a60ea14e65d9bcc7
Abraham Sebastian <abeseb@cs.stanford.edu>
2009-04-12 03:12:47 +0000

    1. Fiz installer - Added "build-installer", "package" and "install" tasks to the main build.xml. - Added the "fiz" shell script for windows and unix/mac. - The app directory contains files that are only part of Fiz web applications. These include README files and application specific ant build files. 2. Moved "lib" and "out" directories one level down: they are both now under the trunk folder. 3. Adding all 3rd party .jar files used by Fiz.

---
+++ bin/fiz.bat
@@ -0,0 +1,75 @@
+@echo off
+
+set FIZ_HOME=@@FIZ_HOME
+set FIZ_REPO=@@FIZ_REPO
+
+if "%1"=="" goto printUsage
+if %1==help goto printUsage
+if %1==create goto create
+if %1==fetch goto fetch
+if %1==upgrade goto upgrade
+if %1==version goto version
+goto printUsage
+
+:create
+call :numargs %*
+if %COUNT% NEQ 2 goto printUsage
+set DEST_DIR=%~f2
+ant -buildfile %FIZ_HOME%\antscripts\create.xml -Dfizhome="%FIZ_HOME%" -Ddestdir="%DEST_DIR%"
+goto end
+
+:fetch
+call :numargs %*
+if %COUNT% NEQ 3 goto printUsage
+set DEST_DIR=%~f3
+ant -buildfile %FIZ_HOME%\antscripts\fetch.xml -Dfizrepository="%FIZ_REPO%" -Dversion="%~2" -Ddestdir="%DEST_DIR%"
+goto end
+
+:upgrade
+call :numargs %*
+if %COUNT%==1 goto synchWithInstaller
+if %COUNT%==2 goto upgradeToVersion
+goto printUsage
+
+:synchWithInstaller
+ant -buildfile antscripts\upgrade.xml -Dfizhome="%FIZ_HOME%"
+goto end
+
+:upgradeToVersion
+ant -buildfile antscripts\upgrade.xml upgradetoversion -Dfizrepository="%FIZ_REPO%" -Dversion="%~2"
+goto end
+
+:version
+call :numargs %*
+if %COUNT%==1 goto fizVersion
+if %COUNT%==2 goto appVersion
+goto printUsage
+
+:fizVersion
+ant -buildfile %FIZ_HOME%/antscripts/version.xml -Dfiz.jar.path="%FIZ_HOME%/lib/fiz.jar"
+goto end
+
+:appVersion
+set ABS_PATH=%~f2
+ant -buildfile %FIZ_HOME%/antscripts/version.xml -Dfiz.jar.path="%ABS_PATH%/lib/fiz.jar"
+goto end
+
+:numargs
+set COUNT=0
+:numargsloop
+if "%1"=="" goto :EOF
+shift
+set /a COUNT+=1
+goto numargsloop
+
+:printUsage
+    echo Usage: fiz [create;upgrade;check]
+	echo.
+	echo Options:
+	echo.
+	echo    help     Print this message and exit.
+	echo    version  Print the version of the Fiz platform or an appliction.
+	echo    create   Create a new Fiz application.
+	echo    upgrade  Upgrade Fiz or an application.
+	echo.
+:end
